86cef9342deef634f4b4f189a5d256fd9996d8d3
Joshua Mosby <jmosby@dhcp-wifi-8021x-155-41-87-243.bu.edu>
2017-06-13 12:36:26 -0400

    index update

--- hw1/routes/index.js
+++ hw1/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.json({“string”: “short”, “length”: 5});
+});
+
+router.post('/', function(req, res, next) {
+    res.json({“string”: “short”, “length”: 5});
 });
 
 module.exports = router;
