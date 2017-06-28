ce2e170814dc52d0ce34dad6eceb731ece2a9b73
Joshua Mosby <jmosby@bu.edu>
2017-06-28 11:31:24 -0400

    Add files via upload

--- hw2/app.js
+++ hw2/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/hw1', assignment);
+app.use('/hw2', assignment);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
